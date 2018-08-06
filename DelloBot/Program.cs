689c1302d34a318f6f7ebcf5a0c692a69e61aa32
DeXoHD <42143537+DeXoHD@users.noreply.github.com>
2018-08-06 15:27:36 +0200

    first commit

---
+++ DelloBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DelloBot
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
